27f9b90758bf4d1093d0333261377d031d7141c7
Shoaib-Khalid-11 <Shoaibkhalid3300@gmail.com>
2026-06-25 20:04:17 +0500

    remove backend from frontend dev

--- Backend/src/index.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app.ts";
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}!`);
-});
--- Backend/src/models/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./userSchema.model.ts";
